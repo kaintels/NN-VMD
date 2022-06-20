bb85f38e91bb4ce4c1726f4bd652c6ff6d18971a
Seungwoo Han <seungwoohan0108@gmail.com>
2022-06-20 11:00:28 +0900

    Update model.py

--- models/model.py
+++ models/model.py
@@ -1,7 +1,6 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch_geometric.nn import GraphConv
 from tensorflow.keras import layers, Model, initializers
 import tensorflow as tf
 
