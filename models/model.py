256f0db11e26f70e5e796bbd9cc506b00ccac54d
Seungwoo Han <swoohan@kakao.com>
2022-09-03 01:06:33 +0900

    Update model.py

--- models/model.py
+++ models/model.py
@@ -1,12 +1,10 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from tensorflow.keras import layers, Model, initializers
-import tensorflow as tf
 
-class VMDNet_TH(nn.Module):
+class VMDNet(nn.Module):
     def __init__(self):
-        super(VMDNet_TH, self).__init__()
+        super(VMDNet, self).__init__()
 
         self.conv1 = nn.Conv1d(1, 32, 3, 1, padding="same")
         self.conv2 = nn.Conv1d(32, 64, 3, 1, padding="same")
@@ -21,25 +19,4 @@ class VMDNet_TH(nn.Module):
         x = self.flatten(x)
         x = self.dense(x)
         x = x.reshape(-1, 3, 140)
-        return x
-
-class VMDNet_TF(Model):
-    def __init__(self):
-        super(VMDNet_TF, self).__init__()
-        kernel_init = initializers.GlorotNormal()
-        bias_init = tf.constant_initializer(0)
-        self.conv1 = layers.Conv1D(32, 3, padding="same",kernel_initializer=kernel_init, bias_initializer=bias_init)
-        self.conv2 = layers.Conv1D(64, 3, padding="same",kernel_initializer=kernel_init, bias_initializer=bias_init)
-        self.flatten = layers.Flatten()
-        self.dense = layers.Dense(420,kernel_initializer=kernel_init, bias_initializer=bias_init)
-        self.reshape = layers.Reshape((-1, 140, 3))
-    
-    def call(self, x):
-        x = self.conv1(x)
-        x = tf.nn.sigmoid(x)
-        x = self.conv2(x)
-        x = tf.nn.sigmoid(x)
-        x = self.flatten(x)
-        x = self.dense(x)
-        x = self.reshape(x)
         return x
