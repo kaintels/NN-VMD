d243f92e9a218437b5f72954ee7fdd7a1589ded0
Seungwoo Han <seungwoohan0108@gmail.com>
2023-02-12 18:27:53 +0900

    Add train.sh

---
+++ train.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+declare MODEL=${1-'mtl'} # cnn / vae / mtl
+declare EPOCH=${2-'200'}
+declare SEED=${3-'123456'}
+
+python main.py --model_name=${MODEL} --epoch=${EPOCH} --seed=${SEED}
