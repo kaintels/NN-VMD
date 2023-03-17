55fc688fed9dca75d16998e97496ba49507b3c29
Seungwoo Han <seungwoohan0108@gmail.com>
2023-03-17 17:25:24 +0900

    Update main -> train.py

--- train.py
+++ train.py
@@ -1,10 +1,9 @@
 import torch
 import torch.nn.functional as F
 import random
-import numpy as np  
-import matplotlib.pyplot as plt  
-import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
 import argparse
 from scipy.io import arff, loadmat
 from sklearn.model_selection import train_test_split
@@ -13,16 +12,16 @@ from models.model import VMDNet, Classifier, TaskLayer, VMD_VAE_DNN
 
 parser = argparse.ArgumentParser(description="Arguments classifiers.")
 parser.add_argument("--model_name", type=str, required=True,
-                        help="model_name.",
-                        dest="model_name")
+                    help="model_name.",
+                    dest="model_name")
 parser.add_argument("--epoch", type=str, required=False,
-                        default="200",
-                        help="epoch numer.",
-                        dest="epoch")
+                    default="200",
+                    help="epoch numer.",
+                    dest="epoch")
 parser.add_argument("--seed", type=str, required=True,
-                        default="123456",
-                        help="seed numer.",
-                        dest="seed")
+                    default="123456",
+                    help="seed numer.",
+                    dest="seed")
 
 args = parser.parse_args()
 
@@ -37,43 +36,55 @@ print(f"EPOCH : {EPOCH}")
 print(f"seed : {seed}")
 
 input_s_tr = arff.loadarff("./dataset/ECG5000_TRAIN.arff")
-input_s_tr = pd.DataFrame(input_s_tr[0], dtype=np.float32).values[:,:140].reshape(500, 1, 140)
+input_s_tr = pd.DataFrame(
+    input_s_tr[0], dtype=np.float32).values[:, :140].reshape(500, 1, 140)
 
 input_l_tr = arff.loadarff("./dataset/ECG5000_TRAIN.arff")
-input_l_tr = pd.DataFrame(input_l_tr[0], dtype=np.float32).values[:,140:141].reshape(500)
+input_l_tr = pd.DataFrame(
+    input_l_tr[0], dtype=np.float32).values[:, 140:141].reshape(500)
 
 input_l_tr = input_l_tr - 1
 
 input_s_ts = arff.loadarff("./dataset/ECG5000_TEST.arff")
-input_s_ts = pd.DataFrame(input_s_ts[0], dtype=np.float32).values[:,:140].reshape(4500, 1, 140)
+input_s_ts = pd.DataFrame(
+    input_s_ts[0], dtype=np.float32).values[:, :140].reshape(4500, 1, 140)
 
 
 input_l_ts = arff.loadarff("./dataset/ECG5000_TEST.arff")
-input_l_ts = pd.DataFrame(input_l_ts[0], dtype=np.float32).values[:,140:141].reshape(4500)
+input_l_ts = pd.DataFrame(
+    input_l_ts[0], dtype=np.float32).values[:, 140:141].reshape(4500)
 
 input_l_ts = input_l_ts - 1
 
-target_s_tr = loadmat("./dataset/processed_train.mat")["data"].reshape(500, 3, 140)
-target_s_ts = loadmat("./dataset/processed_test.mat")["data"].reshape(4500, 3, 140)
+target_s_tr = loadmat(
+    "./dataset/processed_train.mat")["data"].reshape(500, 3, 140)
+target_s_ts = loadmat(
+    "./dataset/processed_test.mat")["data"].reshape(4500, 3, 140)
 
 
 def data_loader(args):
     assert len(args) > 0
     if args == "cnn":
-        train_loader = torch.utils.data.DataLoader(dataset = TrainDataset(input_s_tr, target_s_tr), batch_size = 32, shuffle = True)
-        test_loader = torch.utils.data.DataLoader(dataset = TestDataset(input_s_ts, target_s_ts), batch_size = 32, shuffle = True)
+        train_loader = torch.utils.data.DataLoader(dataset=TrainDataset(
+            input_s_tr, target_s_tr), batch_size=32, shuffle=True)
+        test_loader = torch.utils.data.DataLoader(dataset=TestDataset(
+            input_s_ts, target_s_ts), batch_size=32, shuffle=True)
         model = VMDNet().to(device)
 
     if args == "vae":
-        train_loader = torch.utils.data.DataLoader(dataset = TrainDataset(input_s_tr), batch_size = 32, shuffle = True)
-        test_loader = torch.utils.data.DataLoader(dataset = TestDataset(input_s_ts), batch_size = 32, shuffle = True)
+        train_loader = torch.utils.data.DataLoader(
+            dataset=TrainDataset(input_s_tr), batch_size=32, shuffle=True)
+        test_loader = torch.utils.data.DataLoader(
+            dataset=TestDataset(input_s_ts), batch_size=32, shuffle=True)
         model = VMD_VAE_DNN().to(device)
 
     if args == "mtl":
-        train_loader = torch.utils.data.DataLoader(dataset = TrainDataset(input_s_tr, target_s_tr, input_l_tr), batch_size = 32, shuffle = True)
-        test_loader = torch.utils.data.DataLoader(dataset = TestDataset(input_s_ts, target_s_ts, input_l_ts), batch_size = 32, shuffle = True)
+        train_loader = torch.utils.data.DataLoader(dataset=TrainDataset(
+            input_s_tr, target_s_tr, input_l_tr), batch_size=32, shuffle=True)
+        test_loader = torch.utils.data.DataLoader(dataset=TestDataset(
+            input_s_ts, target_s_ts, input_l_ts), batch_size=32, shuffle=True)
         model = TaskLayer().to(device)
-    
+
     return train_loader, test_loader, model
 
 
@@ -81,13 +92,16 @@ train_loader, test_loader, model = data_loader(args.model_name)
 
 loss_1 = torch.nn.MSELoss()
 loss_2 = torch.nn.CrossEntropyLoss()
+
+
 def custom_fn(outputs, inputs, mu, log_var):
     BCE = F.mse_loss(outputs, inputs, reduction="sum")
     KLD = -0.5 * torch.sum(1 + log_var - mu.pow(2) - log_var.exp())
     return BCE + KLD
 
+
 def train(model, args):
-    optimizer = torch.optim.Adam(model.parameters(), lr = 0.01)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
 
     print("-" * 100)
     alpha = 0.9
@@ -115,10 +129,9 @@ def train(model, args):
                 cost.backward()
                 optimizer.step()
                 avg_cost += cost / len(train_loader)
-            
+
             if i % 10 == 0:
                 print("epoch : {0}, loss : {1}".format(i, avg_cost.item()))
-
 
         if args == "mtl":
             for _, data in enumerate(train_loader):
@@ -140,4 +153,9 @@ def train(model, args):
 
     print("-" * 100)
 
-train(model, args.model_name)
+    return model
+
+
+trained_model = train(model, args.model_name)
+
+torch.save(trained_model.state_dict(), f"./models/{args.model_name}_eph{args.epoch}.pt")
