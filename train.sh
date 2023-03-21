88ae66b44fe1e8c00a72a5e4b458e92f716ff88a
Seungwoo Han <seungwoohan0108@gmail.com>
2023-03-21 12:00:14 +0900

    - main.py -> train.py

--- train.sh
+++ train.sh
@@ -4,4 +4,4 @@ declare MODEL=${1-'mtl'} # cnn / vae / mtl
 declare EPOCH=${2-'200'}
 declare SEED=${3-'123456'}
 
-python main.py --model_name=${MODEL} --epoch=${EPOCH} --seed=${SEED}
+python train.py --model_name=${MODEL} --epoch=${EPOCH} --seed=${SEED}
